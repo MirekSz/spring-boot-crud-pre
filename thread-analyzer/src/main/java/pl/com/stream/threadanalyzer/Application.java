a8c47e8fab473a7c3b54c076327b022eb62a382b
MirekSz <miro1994>
2016-03-09 20:49:08 +0100

    hystrix

--- thread-analyzer/src/main/java/pl/com/stream/threadanalyzer/Application.java
+++ thread-analyzer/src/main/java/pl/com/stream/threadanalyzer/Application.java
@@ -7,9 +7,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
 import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableAsync;
 
+@EnableCircuitBreaker
+@EnableHystrix
+@EnableHystrixDashboard
 @SpringBootApplication
+@EnableAsync
 public class Application {
 
 	public static void main(String[] args) {
