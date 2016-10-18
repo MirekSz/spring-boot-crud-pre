a5f7cd3d7cbfc92e926182e8c68f70a09b860155
MirekSz <m.szajowski@gmail.com>
2016-10-18 14:46:57 +0200

    reczne

--- spring-boot-crud/src/main/java/hello/CustomersController.java
+++ spring-boot-crud/src/main/java/hello/CustomersController.java
@@ -1,11 +1,15 @@
 package hello;
 
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 
 import javax.annotation.PostConstruct;
-import javax.validation.Valid;
+import javax.validation.ConstraintViolation;
+import javax.validation.Validator;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -37,6 +41,7 @@ public class CustomersController {
 	@RequestMapping
 	public String list(Model model) {
 		model.addAttribute("customers", customers.values());
+		model.addAttribute("demo", new Date());
 		return "customer/customer-list";
 	}
 
@@ -48,15 +53,34 @@ public class CustomersController {
 		return "customer/customer-form";
 	}
 
+	@Autowired
+	Validator validator;
+
 	@RequestMapping(value = "/add", method = RequestMethod.POST)
-	public String add(@ModelAttribute @Valid Customer customer, BindingResult bindingResult,
-			RedirectAttributes redirectAttributes) {
+	public String add(@ModelAttribute Customer customer,
+			BindingResult bindingResult, RedirectAttributes redirectAttributes) {
+		handle(customer, bindingResult);
 		if (bindingResult.hasErrors()) {
 			return "customer/customer-form";
 		}
+
 		customer.setId(customers.size() + 1L);
 		customers.put(customer.getId(), customer);
 		redirectAttributes.addFlashAttribute("added", true);
 		return "redirect:/customers";
+	}
+
+	public void handle(Customer customer, BindingResult bindingResult) {
+		Set<ConstraintViolation<Customer>> validate = validator
+				.validate(customer);
+
+		if (validate.size() > 0) {
+			for (ConstraintViolation<Customer> constraintViolation : validate) {
+				bindingResult.rejectValue(constraintViolation.getPropertyPath()
+						.toString(), "NotNull");
+
+			}
+		}
+
 	}
 }
--- spring-boot-crud/src/main/java/hello/StaticResourceConfiguration.java
+++ spring-boot-crud/src/main/java/hello/StaticResourceConfiguration.java
@@ -1,18 +1,44 @@
 package hello;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.format.FormatterRegistry;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
 public class StaticResourceConfiguration extends WebMvcConfigurerAdapter {
 
-	private static final String[] CLASSPATH_RESOURCE_LOCATIONS = { "classpath:/META-INF/resources/",
-			"classpath:/resources/", "classpath:/static/", "classpath:/public/" };
+	private static final String[] CLASSPATH_RESOURCE_LOCATIONS = {
+			"classpath:/META-INF/resources/", "classpath:/resources/",
+			"classpath:/static/", "classpath:/public/" };
 
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
-		registry.addResourceHandler("/**").addResourceLocations(CLASSPATH_RESOURCE_LOCATIONS);
+		registry.addResourceHandler("/**").addResourceLocations(
+				CLASSPATH_RESOURCE_LOCATIONS);
 	}
 
+	@Override
+	public void addFormatters(FormatterRegistry formatterRegistry) {
+		formatterRegistry.addConverter(new Converter<Date, String>() {
+			SimpleDateFormat simpleDateFormat = new SimpleDateFormat(
+					"dd MMMM yyyy HH:mm:ss");
+
+			@Override
+			public String convert(Date arg0) {
+				return simpleDateFormat.format(arg0);
+			}
+		});
+	}
+
+	@Bean
+	public javax.validation.Validator localValidatorFactoryBean() {
+		return new LocalValidatorFactoryBean();
+	}
 }
